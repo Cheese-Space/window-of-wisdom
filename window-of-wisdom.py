3bf69083b328a5a8e06a074aaad7c03a490c1550
Cheese_space <99285740+endgamerYT@users.noreply.github.com>
2025-01-30 17:24:06 +0100

    use messagebox instead of new tkinter window

--- window-of-wisdom.py
+++ window-of-wisdom.py
@@ -1,28 +1,14 @@
 import tkinter as tk
 from subprocess import getoutput
 from os import name
-
+from tkinter import messagebox
 if name == "nt":
-    error = tk.Tk()
-    T2 = tk.Label(text="ERROR: this program doesn't support windows!")
-    T2.pack()
-    button = tk.Button(text="exit", command=error.destroy)
-    button.pack()
-    error.resizable(False, False)
-    error.title("error!")
-    error.mainloop()
+    messagebox.showerror("ERROR", "ERROR: this program doesn't support windows")
 else:
    check_cowsay = getoutput("which cowsay")
    check_fortune = getoutput("which fortune")
    if "which: no cowsay" in check_cowsay or not check_cowsay or "which: no fortune" in check_fortune or not check_fortune:
-       error_window = tk.Tk()
-       error_window.title("error!")
-       T1 = tk.Label(text="ERROR: cowsay or fortune isn't installed, please install before using this program!")
-       T1.pack()
-       button = tk.Button(text="exit", command=error_window.destroy)
-       button.pack()
-       error_window.resizable(False, False)
-       error_window.mainloop()
+       messagebox.showerror("ERROR", "ERROR: cowsay or fortune isn't installed, please install before using!")
    else:
       cow = getoutput("fortune | cowsay -f stegosaurus")
       window = tk.Tk()
