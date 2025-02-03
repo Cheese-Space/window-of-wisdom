b834986b324fcd9f495f157f81ca06411ae8b041
Cheese_space <99285740+endgamerYT@users.noreply.github.com>
2025-02-03 20:47:38 +0100

    added button for new fortune and added flags

--- window-of-wisdom.py
+++ window-of-wisdom.py
@@ -2,19 +2,50 @@ import tkinter as tk
 from subprocess import getoutput
 from os import name
 from tkinter import messagebox
-if name == "nt":
-    messagebox.showerror("ERROR", "ERROR: this program doesn't support windows")
+from tkinter import ttk
+import argparse
+def new_fortune():
+    global outputer
+    cow = getoutput(f"fortune | cowsay {'-f ' + outputer if outputer else ''}")
+    wisdom.config(text=cow)
+parser = argparse.ArgumentParser(description="a window displaying a fortune")
+parser.add_argument("-w", "--skip_nt_error", help="skip windows error", action="store_true")
+parser.add_argument("-t", "--tux", help="makes tux say a fortune", action="store_true")
+parser.add_argument("-c", "--cow", help="makes a cow say a fortune", action="store_true")
+args = parser.parse_args()
+if args.tux:
+    outputer = "tux"
+elif args.cow:
+    outputer = "default"
 else:
-   check_cowsay = getoutput("which cowsay")
-   check_fortune = getoutput("which fortune")
-   if "which: no cowsay" in check_cowsay or not check_cowsay or "which: no fortune" in check_fortune or not check_fortune:
-       messagebox.showerror("ERROR", "ERROR: cowsay or fortune isn't installed, please install before using!")
+    outputer = "stegosaurus"
+if args.skip_nt_error:
+   cow = getoutput(f"fortune | cowsay {'-f ' + outputer if outputer else ''}")
+   window = tk.Tk()
+   wisdom = tk.Label(justify=tk.LEFT, text=cow, font=("Courier", 12))
+   wisdom.pack()
+   button = tk.Button(text="new fortune", command=new_fortune)
+   button.pack()
+   window.title('window of wisdom')
+   window.resizable(False, False)
+   window.mainloop()
+else:
+   if name == "nt":
+       messagebox.showerror("ERROR", "ERROR: this program doens't support windows!")
    else:
-      cow = getoutput("fortune | cowsay -f stegosaurus")
-      window = tk.Tk()
-      wisdom = tk.Label(justify=tk.LEFT, text=cow, font=("Courier", 12))
-      wisdom.pack()
-      window.config(bg="black")
-      window.resizable(False, False)
-      window.title("window of wisdom")
-      window.mainloop()
+      check_cowsay = getoutput("which cowsay")
+      check_fortune = getoutput("which fortune")
+      if "which: no cowsay" in check_cowsay or not check_cowsay or "which: no fortune" in check_fortune or not check_fortune:
+          messagebox.showerror("ERROR", "cowsay or fortune isn't installed, please install before using this program")
+      else:
+         cow = getoutput(f"fortune | cowsay {'-f ' + outputer if outputer else ''}")
+         window = tk.Tk()
+         wisdom = tk.Label(justify=tk.LEFT, text=cow, font=("Courier", 12))
+         wisdom.pack()
+         button = ttk.Button(text="new fortune", command=new_fortune)
+         button.pack()
+         window.resizable(False, False)
+         window.title("window of wisdom")
+         window.mainloop()
+
+
